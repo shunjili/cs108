2fbe3679f7789f418def832b5ae5895bd685a8b4
JB Bakst <jbakst@stanford.edu>
2014-03-01 15:53:59 -0800

    Question Type enum in Question interface

--- WebQuiz/src/objects/Question.java
+++ WebQuiz/src/objects/Question.java
@@ -1,5 +1,7 @@
 package objects;
 
-public interface Question {
+import objects.Account.Type;
 
+public interface Question {
+	public enum Type{QUESTION_RESPONSE, FILL_IN_BLANK, MULT_CHOICE, PIC_RESPONSE};
 }
