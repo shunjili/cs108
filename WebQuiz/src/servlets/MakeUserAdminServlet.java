1bf664f74f9141c747e5dae3fa96a2ef60f0b94f
JB Bakst <jbakst@stanford.edu>
2014-03-13 23:57:25 -0700

    Make admin implemented

--- WebQuiz/src/servlets/MakeUserAdminServlet.java
+++ WebQuiz/src/servlets/MakeUserAdminServlet.java
@@ -1,11 +1,14 @@
 package servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import objects.AccountManager;
 
 /**
  * Servlet implementation class MakeUserAdminServlet
@@ -33,7 +36,10 @@ public class MakeUserAdminServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		AccountManager.makeUserAdmin(username);
+		request.getRequestDispatcher("Administration.jsp").forward(request, response);
+		return;
 	}
 
 }
