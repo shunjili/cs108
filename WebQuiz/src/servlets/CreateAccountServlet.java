cac6a188ce9163c3ab3fe0a3fb66ac71689030c3
JB Bakst <jbakst@stanford.edu>
2014-03-13 17:06:22 -0700

    account creation forwards to homepage if successful, otherwise shows the form again

--- WebQuiz/src/servlets/CreateAccountServlet.java
+++ WebQuiz/src/servlets/CreateAccountServlet.java
@@ -2,12 +2,14 @@ package servlets;
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+import javax.servlet.http.HttpSession;
+
 import objects.Account;
 import objects.AccountManager; 
 
@@ -17,6 +19,10 @@ import objects.AccountManager;
 @WebServlet("/CreateAccountServlet")
 public class CreateAccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public static final String INVALID_USERNAME = "invalid_username";
+	public static final String INVALID_PASSWORD = "invalid_password";
+	public static final String PASSWORDS_DO_NOT_MATCH = "passwords_do_not_match";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -48,17 +54,31 @@ public class CreateAccountServlet extends HttpServlet {
 		if(!password1.equals(password2)) {
 			//if not, set success = false, and forward to the result page immediately
 			request.setAttribute("success", success);
-			request.getRequestDispatcher("CreateAccountUnsuccessful.html").forward(request, response);
+			request.getRequestDispatcher("CreateAccountResult.jsp").forward(request, response);
+			return;
+		} else if (password1.equals("")) {
+			request.setAttribute("success", success);
+			request.getRequestDispatcher("CreateAccountResult.jsp").forward(request, response);
+			return;
+		} else if (username.equals("")) {
+			request.setAttribute("success", success);
+			request.getRequestDispatcher("CreateAccountResult.jsp").forward(request, response);
 			return;
 		}
 
 		//now, try storing the account
 		Account newAccount = new Account(username);
 		success = AccountManager.storeNewAccount(newAccount, password1);
-		request.setAttribute("success", success);
-		request.setAttribute("newAccount", newAccount);
-		request.getRequestDispatcher("CreateAccountResult.jsp").forward(request, response);
-
-
+		if (success) {
+			Account loggedAccount = AccountManager.getAccountLogin(username, password1);
+			request.setAttribute("loggedAccount", loggedAccount);
+			HttpSession session = request.getSession();
+			session.setAttribute("loggedAccount", loggedAccount);
+			request.getRequestDispatcher("ViewMyAccount.jsp").forward(request, response);
+		} else {
+			request.setAttribute("success", success);
+			request.getRequestDispatcher("CreateAccountResult.jsp").forward(request, response);
+			return;
+		}
 	}
 }
