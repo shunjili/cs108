d580966f59d814c4c4e42f298b5385e03625cd9d
Shuhui Qu <AgentQ@dn0a2253b7.sunet>
2014-03-14 18:09:45 -0700

    modify multiple choice with multiple answer

--- WebQuiz/src/objects/MultipleChoicewithMultipleAnswers.java
+++ WebQuiz/src/objects/MultipleChoicewithMultipleAnswers.java
@@ -1,20 +1,183 @@
 package objects;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MultipleChoicewithMultipleAnswers extends MultipleChoiceQuestion{
+import objects.Question.Type;
+
+public class MultipleChoicewithMultipleAnswers implements Question{
+	
+	private String questionID;
+	private String question;
+	private String questionPrefix;
+	private int description;
+	private String creator_id;
+	private Question.Type type;
+	private int score;
+	private Timestamp timestamp;
+	private String[] choices;
+	
 	
 	public MultipleChoicewithMultipleAnswers(String questionID, String question, String description,
 			String creator_id, int score, Timestamp timestamp) {
-		super( questionID,  question,  description,
-				 creator_id,  score,  timestamp);
+		this.questionID = questionID;	
+		parseQuestionAndChoices(question);
+		this.description = Integer.parseInt(description);
+		this.creator_id = creator_id;
+		this.type = Question.Type.MULTIPLE_CHOICE_MULTIPLE_ANSWER;
+		this.score = score;
+		this.timestamp = timestamp;
+	}
+	public boolean parseQuestionAndChoices(String questionString){
+		String[] stringArray = questionString.split("#");
+		question = questionString;
+		if(stringArray.length < 2){
+			questionPrefix = questionString;
+			choices = null;
+			return false;
+		}else{
+			questionPrefix = stringArray[0];
+			choices = Arrays.copyOfRange(stringArray, 1, stringArray.length);
+			return true;
+		}
+	}
+	
+	public int getNumberOfChoices(){
+		return choices.length;
+	}
+	
+	public String getQuestionID() {
+		return this.questionID;
+	}
+	
+	public void setID(int id) {
+		this.questionID = "" + id;
+	}
+	
+	public String getQuestion() {
+		return this.question;
+	}
+	
+	public String getDescription() {
+		return Integer.toString(this.description);
+	}
+	
+	public String getCreatorID() {
+		return this.creator_id;
 	}
 	
 	public Question.Type getType() {
-		return Question.Type.MULTIPLE_CHOICE_MULTIPLE_ANSWER;
+		return this.type;
 	}
 	
 	public String getTypeString() {
 		return "MULTIPLE_CHOICE_MULTIPLE_ANSWER";
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+	
+	public String getTimestampString() {
+		return this.timestamp.toString();
+	}
+
+	public String getHTML(boolean answer) {
+		String html =  " <div class=\"panel-body\">"+ questionPrefix;
+
+		if(choices != null){
+			for (int i = 0; i < choices.length; i++){
+				html +=String.format("<div class=\"input-group\"><span class=\"input-group-addon\"><input type=\"checkbox\" name = %s value = %d></span><label type=\"text\" class=\"form-control\">%s</label></div><br>", questionID, i+1, choices[i]) ;
+			}
+		}
+		
+		html += "</div>";
+		if(answer){
+			ArrayList<String> answers = QuestionManager.getAnswers(questionID);
+			if(answers != null){
+				for(int i = 0; i < answers.size(); i++){
+					html += String.format("<div class=\"panel-footer\">%s</div>", answers.get(i));
+				}
+			}
+		}
+		return html;
+	}
+	
+	public boolean isCorrect(ArrayList<String> userAnswers){
+		if(userAnswers==null || userAnswers.size() <=0||userAnswers.contains(null)||userAnswers.size() < description){
+			return false;
+		}else{
+			//ArrayList<String> answers = new ArrayList<String>();
+			//answers = QuestionManager.getAnswers(questionID);
+			ArrayList<String> answers = new ArrayList<String>();
+			ArrayList<String> answertemp = new ArrayList<String>();
+			answertemp = QuestionManager.getAnswers(questionID);
+			for(int i = 0; i < answertemp.size(); i++){
+				answers.add(answertemp.get(i).toLowerCase());
+			}
+			//check answer might have multiple user answers 
+			int len = userAnswers.size();
+			boolean flag = true;
+			for(int i = 0; i < len; i++){
+				// if any of the solutions are not in the answer then it's false
+				//String temp = userAnswers.get(i);
+				//System.out.println(temp);
+				//temp = temp.trim();
+				//System.out.println(temp);
+				if(!answers.contains(userAnswers.get(i).trim())){
+					System.out.println(userAnswers.get(i));
+					flag = false;
+					break;
+				}
+			}		
+			//return flag
+			return flag;
+		}
+	}
+
+	public int getScore(ArrayList<String> userAnswers){
+		if(isCorrect(userAnswers)){
+			return score;
+		}
+		return 0;
+		
+	}
+	public String getResultView(ArrayList<String> userAnswers){
+		
+		ArrayList<String> answers = new ArrayList<String>();
+		answers = QuestionManager.getAnswers(questionID);
+		
+		String htmlContent = "<div class=\"panel-body\">" + questionPrefix;
+		htmlContent += "<table class=\"table\">";
+		
+		htmlContent += "<thead><tr><th>Correct Answer</th><th>Your Answer</th></tr></thead>";
+		htmlContent += "<tbody><tr><td>";
+
+		for(int i = 0; i < answers.size(); i++){
+//			htmlContent += Integer.toString(i + 1) + ".";
+			htmlContent += choices[Integer.parseInt(answers.get(i))-1];
+			htmlContent += "&nbsp;";
+		}		
+		htmlContent += "</td><td>";
+
+		if(userAnswers == null||userAnswers.contains(null)){
+			htmlContent += "";
+			htmlContent += "&nbsp;"; 
+		}else{
+			for(int i = 0; i < userAnswers.size(); i++){;
+				htmlContent += choices[Integer.parseInt(userAnswers.get(i)) - 1];
+				htmlContent += "&nbsp;";
+			}	
+
+		}
+		htmlContent += "</td></tr></tbody></table>";
+		htmlContent +="</div>";
+		return htmlContent;
+		
 	}
 }
